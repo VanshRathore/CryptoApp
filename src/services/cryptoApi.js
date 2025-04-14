6d9a9bf722c7d939c5658b63082ca035be8f43ed
Vansh Rathore <vanshr083@gmail.com>
2025-04-14 22:09:54 +0530

    completed the layout of app

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'; 
+
+const cryptoApiHeaders = {
+    'x-rapidapi-key': 'fa42acac64msh33caa01b940198cp130cfbjsn1e6894c1c7a8',
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com'
+};
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/stats';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/exchanges'),
+        }),
+    }),
+})
